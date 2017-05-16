e0ae34ae8a93f0212f7b058aaa82827b354444f5
daniel same <lesamessipa@gmail.com>
2017-05-17 01:38:40 +0200

    modif aeroport

--- pages/aeroport.php
+++ pages/aeroport.php
@@ -1,38 +1,68 @@
 
-<div id="aile">
-    <h1>Trajet aéroport</h1>
-    <form class="formulaire" method="post" action="#"><br>
-      <label for="origine">Partir de:</label><br>
-      <input type="text" name="origine" id="origine"><br>
-      <label for="destination">Aller à:</label><br>
-      <input type="text" name="destination" id="destination"><br>
-      <label for="date">Date:</label><br>
-      <input type="date" name="" value="" placeholder="jj/mm/aaaa"><br>
-      <label for="heure">Horaire:</label>
-      <select class="horaires" name="horaires">
-        <option value="06:00">06:00</option>
-        <option value="07:00">07:00</option>
-        <option value="08:00">08:00</option>
-        <option value="09:00">09:00</option>
-        <option value="10:00">10:00</option>
-        <option value="11:00">11:00</option>
-        <option value="12:00">12:00</option>
-        <option value="13:00">13:00</option>
-        <option value="14:00">14:00</option>
-        <option value="15:00">15:00</option>
-        <option value="16:00">16:00</option>
-        <option value="17:00">17:00</option>
-        <option value="18:00">18:00</option>
-        <option value="19:00">19:00</option>
-        <option value="20:00">20:00</option>
-        <option value="21:00">21:00</option>
-        <option value="22:00">22:00</option>
-        <option value="23:00">23:00</option>
-        <option value="00:00">00:00</option>
-      </select><br>
-      <input id="submit" type="submit">
-    </form>
-</div>
+          <div id="aile">
+             <h1>Trajet aéroport</h1>
+             <form class="formulaire2" method="post" action="#"><br>
+                  <h3> mes informations </h3>
+                  <label for="name">nom* :</label>
+                  <input type="text" name="nom" value="" required="" placeholder="entrez votre nom" id='name'>
+                  <label for="firstname"> prenom* :</label>
+                  <input type="text" name="firstname" value="" required="" placeholder="entrez votre prenom" id='firstname'><br>
+                  <label for="email">email* :</label>
+                  <input type="email" name="mail" value="" required="" placeholder="ex:driver@gmail.com">
+                  <h3> mon trajet </h3>
+                  <label for="go"> lieu de depart* :</label>
+                  <input type="text" name="" value="" id="go" required="" placeholder="entrez votre lieu de depart">
+                  ou aeroport:
+                  <select value="choisir un aeroport">
+                    <option value="1">choississez</option>
+                    <option value="2"> aeroport charles Degaule</option>
+                    <option value="3"> aeroport orly(sud)</option>
+                  </select><br>
+                  <label for="arriver"> lieu d'arriver* :</label>
+                  <input type="text" name="" value="" id="arriver" placeholder="entrez votre lieu d'arrivé">
+                  <label for="selection" id='choix2'>ou aeroport :</label>
+                  <select class="two" name="aeroports" id='selection'>
+                    <option value="1">choississez</option>
+                    <option value="2">aeroport charles degaule</option>
+                    <option value="3">aeroport orly</option>
+                  </select><br>
+                  <label for="">etape dans le trajet :</label>
+                   <input type="radio" class="radio" name="choix" value="oui"> oui
+                   <input type="radio" class="radio" name="choix" value="non"> non <br>
+                   <label for="personne"> nombres de personnes :</label>
+                   <select class="personnes" name="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                   </select> |
+                   <label for="horaires">heure de depart :</label>
+                   <select class="horaires" name="horaires">
+                     <option value="06:00">06:00</option>
+                     <option value="07:00">07:00</option>
+                     <option value="08:00">08:00</option>
+                     <option value="09:00">09:00</option>
+                     <option value="10:00">10:00</option>
+                     <option value="11:00">11:00</option>
+                     <option value="12:00">12:00</option>
+                     <option value="13:00">13:00</option>
+                     <option value="14:00">14:00</option>
+                     <option value="15:00">15:00</option>
+                     <option value="16:00">16:00</option>
+                     <option value="17:00">17:00</option>
+                     <option value="18:00">18:00</option>
+                     <option value="19:00">19:00</option>
+                     <option value="20:00">20:00</option>
+                     <option value="21:00">21:00</option>
+                     <option value="22:00">22:00</option>
+                     <option value="23:00">23:00</option>
+                     <option value="00:00">00:00</option>
+                   </select><br>
+                   <input type="submit" id='validation'name="" value="valider ma commande">
+
+                  
+             </form>
+        </div>
 <div class="descriptif">
   <h1>Trajet aéroport</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
