d3a788ad4eb740406dd275f29b95aa254d97ae00
sambou <samboudiallo@ymail.com>
2017-05-16 16:32:08 +0200

    aeroport

--- pages/aeroport.php
+++ pages/aeroport.php
@@ -1,16 +1,39 @@
 
-
-
-<div id="aile" class="parallax-container">
-    <div class="parallax"><img  style="display: block;" src="../images/avion.jpeg"></div>
+<div id="aile">
+    <h1>Trajet aéroport</h1>
+    <form class="formulaire" method="post" action="#"><br>
+      <label for="origine">Partir de:</label><br>
+      <input type="text" name="origine" id="origine"><br>
+      <label for="destination">Aller à:</label><br>
+      <input type="text" name="destination" id="destination"><br>
+      <label for="date">Date:</label><br>
+      <input type="date" name="" value="" placeholder="jj/mm/aaaa"><br>
+      <label for="heure">Horaire:</label>
+      <select class="horaires" name="horaires">
+        <option value="06:00">06:00</option>
+        <option value="07:00">07:00</option>
+        <option value="08:00">08:00</option>
+        <option value="09:00">09:00</option>
+        <option value="10:00">10:00</option>
+        <option value="11:00">11:00</option>
+        <option value="12:00">12:00</option>
+        <option value="13:00">13:00</option>
+        <option value="14:00">14:00</option>
+        <option value="15:00">15:00</option>
+        <option value="16:00">16:00</option>
+        <option value="17:00">17:00</option>
+        <option value="18:00">18:00</option>
+        <option value="19:00">19:00</option>
+        <option value="20:00">20:00</option>
+        <option value="21:00">21:00</option>
+        <option value="22:00">22:00</option>
+        <option value="23:00">23:00</option>
+        <option value="00:00">00:00</option>
+      </select><br>
+      <input id="submit" type="submit">
+    </form>
 </div>
-
-<div style="position: absolute; top: 200px; left: 320px; z-index: 5;"><h3>TRANSPORT AEROPORT</h3>
-	<p>sdfgsdfiygyu<br>
-	dfgsdfiygyu<br>
-	dfgsdfiygyu<br>
-	dfgsdfiygyu<br>
-	dfgsdfiygyu<br>
-	dfgsdfiygyu
-	</p>
+<div class="descriptif">
+  <h1>Trajet aéroport</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
